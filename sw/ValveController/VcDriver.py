1c3441e3ad5abda387f8a870b5e1c7b6028e2241
Marek Koza <qyx@krtko.org>
2014-12-17 19:01:18 +0100

    Add valve controller driver class.

---
+++ sw/ValveController/VcDriver.py
@@ -0,0 +1,29 @@
+
+class VcDriver(object):
+
+	def __init__(self):
+		pass
+
+	def connect(self, ip, port):
+		pass
+
+	def disconnect(self):
+		pass
+
+	def setHilight(self, valves):
+		pass
+
+	def addHilight(self, valves):
+		pass
+
+	def delHilight(self, valves):
+		pass
+
+	def setValves(self, valves):
+		pass
+
+	def openValves(self, valves):
+		pass
+
+	def closeValves(self, valves):
+		pass
